7b46c2b011868ff82568a53f5bf0a776d5661afb
minorcell <minorcell6789@gmail.com>
2024-09-06 15:18:48 +0800

    delete: 删除了node_modules

--- CP_Back/src/Dao/UserDao.js
+++
@@ -1,8 +0,0 @@
-//UserDao.js
-const db = require("../common/utils/db");
-class UserDao {
-  async getUserList() {
-    return await db.select("*").from("user");
-  }
-}
-module.exports = new UserDao();
--- CP_Back/src/Service/userService.js
+++
@@ -1,9 +0,0 @@
-// UserService.js
-const UserDao = require("../dao/UserDao");
-
-class UserService {
-    async getUserList(){
-        return await UserDao.getUserList();
-    }
-}
-module.exports = new UserService();
