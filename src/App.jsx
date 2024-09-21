c03e284eccab91a1a4e6166bb556349f92034827
achamster <14425769+achamster@user.noreply.gitee.com>
2024-09-21 17:21:26 +0800

    refactor: 代码规范

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import RouteConfig from './router/index.jsx';
-import {NavLink} from "react-router-dom";
-import * as style from './styles/test.module.css'
+import React from 'react';
+import RouteConfig from './router/index';
 
 export default function App() {
-
-    return (
-        <>
-            <div className={style.header}>
-                <NavLink to="/notfound">
-                    <h1>NotFound</h1>
-                </NavLink>
-                <NavLink to="/havefound">
-                    <h1>HaveFound</h1>
-                </NavLink>
-            </div>
-            <div className={style.body}>
-                <RouteConfig></RouteConfig>
-            </div>
-        </>
-    )
+  return (
+    <div>
+      <RouteConfig />
+    </div>
+  );
 }
